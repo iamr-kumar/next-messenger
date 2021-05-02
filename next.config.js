b0b0f47498e6f64df556ab581e20bfafe853a3b4
Ritik <ritik.kumar006@gmail.com>
2021-05-03 01:36:11 +0530

    Revert commmit"
    This reverts commit f392c75d981f473a51150d0ad5bd49d93569ea9a.

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    FIREBASE_API_KEY: "AIzaSyAWU14R64X2IZOpoh-ESjDXgoxyt9ShVK0",
+    FIREBASE_AUTH_DOMAIN: "next-messenger-65b48.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "next-messenger-65b48",
+    FIREBASE_STORAGE_BUCKET: "next-messenger-65b48.appspot.com",
+    FIREBASE_MESSAGE_SENDER_ID: "893335443391",
+    FIREBASE_APP_ID: "1:893335443391:web:063c6c918488b9d4b2730a",
+  },
+};
